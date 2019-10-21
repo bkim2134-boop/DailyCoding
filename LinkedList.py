f90fe33fae8d87d55095bd5aefe0c1dbb8c3fde9
Benjamin Kim <51447047+bkim2134-boop@users.noreply.github.com>
2019-10-20 23:06:38 -0500

    something something

---
+++ LinkedList.py
@@ -0,0 +1,10 @@
+class Node:
+    data = 0 
+    node_next = None
+
+    def __init__(self, data, node_next):
+        self.data = data
+        self.node_next = node_next
+
+#going to use node for tree as well
+#this is to ge faster and familiarize myself
