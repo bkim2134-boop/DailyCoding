cae3f39d949b86a630c47aa58ef7df420b469042
Benjamin Kim <51447047+bkim2134-boop@users.noreply.github.com>
2019-10-20 23:50:02 -0500

    nested class

--- LinkedList.py
+++ LinkedList.py
@@ -1,10 +1,30 @@
-class Node:
-    data = 0 
-    node_next = None
 
-    def __init__(self, data, node_next):
-        self.data = data
-        self.node_next = node_next
 
+class LinkedList:
+    head = None
+    
+    class Node:
+        data = 0 
+        node_next = None
+        
+
+        def __init__(self, data):
+            self.data = data
+            pass
+            
+    def __init__(self):
+
+        self.head = None
+    
+    def add_node(self, data):
+        temp = self.Node(data)
+        if(self.head == None):
+            self.head = temp
+        else:
+            #iterate till end and then append
+            return
+
+LL1 = LinkedList()        
+LL1.add_node(10)
 #going to use node for tree as well
 #this is to ge faster and familiarize myself
