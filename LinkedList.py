5cbd5309980bdcac2af050a3b518521032df54a9
Benjamin Kim <51447047+bkim2134-boop@users.noreply.github.com>
2019-10-21 00:09:50 -0500

    added append and print

--- LinkedList.py
+++ LinkedList.py
@@ -21,10 +21,22 @@ class LinkedList:
         if(self.head == None):
             self.head = temp
         else:
-            #iterate till end and then append
-            return
+            temp_1 = self.head
+            while(temp_1.node_next != None):
+                temp_1 = temp_1.node_next
+            temp_1.node_next = temp    
+                #iterate till end and then append
+    def printList(self):
+        temp = self.head
+        while(temp != None):
+            print(temp.data)
+            temp = temp.node_next        
 
 LL1 = LinkedList()        
 LL1.add_node(10)
+LL1.add_node(12)
+LL1.add_node(5)
+
+LL1.printList()
 #going to use node for tree as well
 #this is to ge faster and familiarize myself
